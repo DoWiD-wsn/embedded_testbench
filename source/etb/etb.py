f746a38edd6d685146f3a944cf8a84dcfef6b703
Dominik Widhalm <widhalm@technikum-wien.at>
2021-03-30 16:01:45 +0200

    Started to prepare Python library/sources

---
+++ source/etb/etb.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+# Just for test ... for now
+print("Here I am ...")
---
+++ source/examples/power_monitor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Add path to the etb module
+import sys
+sys.path.insert(1, '../etb/')
+# Import the etb module
+import etb
