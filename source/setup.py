41af8ba754abdeb75534fcdee0f36f93a3c0de45
Dominik Widhalm <widhalm@technikum-wien.at>
2021-04-01 11:26:16 +0200

    Updated setup script

--- source/setup.py
+++ source/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='etb',
+    name='ETB',
     version='0.1',
     description='Support package for the Embedded Testbench (ETB)',
     url='https://github.com/DoWiD-wsn/embedded_testbench',
     author='Dominik Widhalm',
     author_email='widhalm@technikum-wien.at',
     license='MIT',
-    packages=['etb'],
+    packages=['ETB'],
     zip_safe=False
 )
